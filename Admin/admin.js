e9a89dc32d3cb807d8d27826520331d140caabcf
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-02-05 03:05:18 +0800

    almost working company organisers

--- Admin/admin.js
+++ Admin/admin.js
@@ -19,11 +19,33 @@ $(document).ready(function() {
 		},
 		success: function(data) {
 			$.each(data,function(index,value) {
+
+				var dropdown;
+
+				$.ajax({
+					url: prefix + 'admin/organiser/all',
+					type: 'GET',
+					dataType: 'json',
+					async: false,
+					data: {
+						admin: username,
+						basicPassword: password
+					},
+					success: function(data_org) {
+						$.each(data_org,function(index,val_org) {
+							if (value.id == val_org.id) {
+								console.log(val_org);
+								dropdown+='<li class="item">'+val_org.email+'</org>';
+							}
+						});
+					}
+				});
+
 				var card = '<div class="card-box"><div class="card-content"><h3 class="title">'+value.name+
-								'</h3><div grid><div column="10"><p class="content">'+value.company+'</p></div>'+
-								'<div column="1">Hello</div>'+
+								'</h3><div grid><div column="11"><p class="content">'+dropdown+'</p></div>'+
 								'<div column="1"><button class="-error handleDelete" id="cmp'+value.id+'"><i class="fa fa-times" aria-hidden="true"></i>'+
 								'</button></div></div></div></div></div>';
+				
 
 				$('#adminPanel').append(card);
 			})
@@ -62,12 +84,10 @@ $(document).ready(function() {
 		var id = this.id.substring(3,this.id.length );
 
 		$.ajax({
-			url: prefix + 'admin/company/'+id,
-			type: 'DELETE',
-			data: {
-				admin: username,
-				basicPassword: password,
-			},
+			url: prefix + 'admin/company/'+id+'?admin='+username+'&basicPassword='+password,
+			type: 'DELETE'
+		}).fail(function(e) {
+			console.log(e.responseText);
 		});
 
 		getData();
