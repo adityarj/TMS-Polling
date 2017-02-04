1187bce8edca0c41818ab117166ff1162e42a7da
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-02-05 02:15:24 +0800

    changed delete

--- Admin/admin.js
+++ Admin/admin.js
@@ -20,7 +20,10 @@ $(document).ready(function() {
 		success: function(data) {
 			$.each(data,function(index,value) {
 				var card = '<div class="card-box"><div class="card-content"><h3 class="title">'+value.name+
-								'</h3><p class="content">'+value.company+'</p></div></div></div>';
+								'</h3><div grid><div column="10"><p class="content">'+value.company+'</p></div>'+
+								'<div column="1">Hello</div>'+
+								'<div column="1"><button class="-error handleDelete" id="cmp'+value.id+'"><i class="fa fa-times" aria-hidden="true"></i>'+
+								'</button></div></div></div></div></div>';
 
 				$('#adminPanel').append(card);
 			})
@@ -53,7 +56,20 @@ $(document).ready(function() {
 		});
 
 		getData();
-	})
+	});
 	
-	
+	$(document).on('click','.handleDelete',function() {
+		var id = this.id.substring(3,this.id.length );
+
+		$.ajax({
+			url: prefix + 'admin/company/'+id,
+			type: 'DELETE',
+			data: {
+				admin: username,
+				basicPassword: password,
+			},
+		});
+
+		getData();
+	});
 })
