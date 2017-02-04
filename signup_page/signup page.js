72016bb5194f075fdbd6c4eb65a9895ed5e3f0d2
pappar1027 <zou_yun@mymail.sutd.edu.sg>
2017-02-04 22:31:40 +0800

    new pages added

--- signup_page/signup page.js
+++ signup_page/signup page.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         next_fs = $(this).parent().next();
 
         //activate next step on progressbar using the index of next_fs
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+        //  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
         //show the next fieldset
         next_fs.show();
