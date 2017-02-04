c64f78f6d1eea9799775b9d0c52b6505a4dcc9d9
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-02-05 01:31:30 +0800

    added adding functionality

--- Admin/admin.js
+++ Admin/admin.js
@@ -1,22 +1,59 @@
 $(document).ready(function() {
 
 	var prefix = 'http://10.12.54.167:8080/api/';
-	var password = 'admin';
-	var username = 'password';
+	var password;
+	var username;
 
 
-	$.ajax({
+	function getData() {
+
+		$('#adminPanel').empty();
+		
+		$.ajax({
 		url: prefix + 'admin/company/all',
 		type: 'GET',
 		dataType: 'json',
 		data: {
-			admin: 'admin',
-			password: 'password'
+			admin: username,
+			basicPassword: password
 		},
 		success: function(data) {
-			console.log(data);
-		}
+			$.each(data,function(index,value) {
+				var card = '<div class="card-box"><div class="card-content"><h3 class="title">'+value.name+
+								'</h3><p class="content">'+value.company+'</p></div></div></div>';
+
+				$('#adminPanel').append(card);
+			})
+		}});
+	}
+
+	$('#logButton').click(function() {
+		password = $('#passwordInput').val();
+		username = $('#adminInput').val();
+
+		getData();		
 	});
+
+	$('#addButton').click(function() {
+
+		var compName = $('#companyInput').val();
+
+		$.ajax({
+			url: prefix + 'admin/company/',
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				admin: username,
+				basicPassword: password,
+				name: compName
+			},
+			success: function(data) {
+				console.log('success');
+			}
+		});
+
+		getData();
+	})
 	
 	
 })
